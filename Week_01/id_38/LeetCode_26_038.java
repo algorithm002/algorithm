347d8907ed512e7b1163c64e74c4eee407fae0fb
jianbo liu <jianboliu@jianbodeMacBook-Air.local>
2019-06-04 22:40:03 +0800

    update leetcode 26

--- Week_01/id_38/LeetCode_26_038.java
+++ Week_01/id_38/LeetCode_26_038.java
@@ -7,63 +7,26 @@
  */
 class LeetCode_26_038 {
 
-    /** 
-     * 不考虑原地删除的情况，只求新数组的长度
-     * 写出来发现真是又臭又长
-     */
-    public int removeDuplicates2(int[] nums) {
-        // 检查数据
-        if (nums.length == 0) {
-            return 0;
-        }
-
-        // 处理
-        int tmp = nums[0];
-        int[] newnums = new int[nums.length];
-        newnums[0] = tmp;
-        int newnumsLength = 1;
-        for (int i = 1; i <  nums.length; ++i) {
-            if (tmp != nums[i]) {
-                newnums[newnumsLength] = nums[i];
-                newnumsLength++;
-            } 
-            tmp = nums[i];
-        }
-
-        for(int i = 0; i < newnums.length; ++i) {
-            System.out.print(newnums[i] + ",");
-        }
-
-        return newnumsLength;
-    }
-
     public int removeDuplicates(int[] nums) {
         // check valid
-        if (nums.length == 0) {
-            return 0;
-        }
-        // process
+        if (nums.length == 0) return 0;
+
+        //process
         int i = 0;
         for (int j = 1; j < nums.length; ++j) {
             if (nums[i] != nums[j]) {
                 i++;
                 nums[i] = nums[j];
-            } 
+            }
         }
-        return i+1;    
+
+        return i + 1;
     }
 
     public static void main(String[] args) {
         LeetCode_26_038 testCode_26_038 = new LeetCode_26_038();
         int[] nums = new int[]{1, 1, 2, 2, 3};
-        int newnumsLength = testCode_26_038.removeDuplicates2(nums);
-        System.err.println(newnumsLength);
-
-        int i = 0;
-        for (; i < 5; ++i) {
-            System.out.println("i=" + i);
-        }
-        System.out.println(" at last i=" + i);
+        System.out.println(testCode_26_038.removeDuplicates2(nums););
     }
 }
 
