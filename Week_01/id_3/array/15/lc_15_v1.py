9ca3c969db7ba4bdc2e8efdf505d00a364356889
yujingwen <lection.yu@gmail.com>
2019-06-06 14:09:33 +0800

    15 进一步调整代码 优化剪枝 1120 ms 64.83%

--- Week_01/id_3/array/15/lc_15_v1.py
+++ Week_01/id_3/array/15/lc_15_v1.py
@@ -11,10 +11,15 @@ def three_sum(nums):
     for n in nums:
         count = 1 + cache.get(n, 0)
         cache[n] = count
-        if n == 0 and count <= 3:
+        if n and count <= 2:
             new_nums.append(n)
-        elif count <= 2:
-            new_nums.append(n)
+
+    zero_count = cache.get(0, 0)
+    if zero_count >= 3:
+        r.append([0, 0, 0])
+
+    if zero_count > 0:
+        new_nums.append(0)
 
     nums = new_nums
     nums.sort()
@@ -26,19 +31,19 @@ def three_sum(nums):
             n2 = nums[j]
             if n1 == 0 and n2 == 0 and cache[0] < 3:
                 continue
-            s = n1 + n2
+            s = -(n1 + n2)
 
-            # 已排序的数组，剪掉重复的计算。
-            if s > 0:
+            # 迭代顺序是从小到大的
+            # 当-sum(n1,n2)的结果小于n2时 说明已经迭代出现了重复 -sum必然在之前已经迭代过
+            if s < n2:
                 break
 
-            if (-s == n1 or -s == n2) and cache.get(-s, 0) == 1:
+            if (s == n1 or s == n2) and cache.get(s, 0) == 1:
                 continue
 
-            if cache.get(-s, 0):
-                arr = [n1, n2, -s]
-                arr.sort()
-                ds = ','.join(map(str, arr))
+            if cache.get(s, 0):
+                arr = [n1, n2, s]
+                ds = '%s,%s' % (n1, n2)
                 if ds not in dset:
                     dset.add(ds)
                     r.append(arr)
