18b825fdd71b3b42c3fd5befc6f3fb243552c765
ChenYue <liveforexperience@126.com>
2019-06-06 22:52:02 +0800

    提交441题1解

---
+++ Week_01/id_18/LeetCode_441_18.java
@@ -0,0 +1,11 @@
+package Week_01.id_18;
+
+/**
+ * @author LiveForExperience
+ * @date 2019/6/6 13:30
+ */
+public class LeetCode_441_18 {
+    public int arrangeCoins(int n) {
+        return (int)(Math.sqrt(n * 2.0 + 0.25) - 0.5);
+    }
+}
