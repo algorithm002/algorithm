1c8f0e4a0531d22fc69ee2ae6dab60f3aa8969b9
ChenYue <liveforexperience@126.com>
2019-06-06 23:15:14 +0800

    增加441题1解

--- Week_01/id_18/LeetCode_441_18.java
+++ Week_01/id_18/LeetCode_441_18.java
@@ -8,4 +8,12 @@ public class LeetCode_441_18 {
     public int arrangeCoins(int n) {
         return (int)(Math.sqrt(n * 2.0 + 0.25) - 0.5);
     }
+
+    public int arrangeCoins1(int n) {
+        int c = 0;
+        while(c < n) {
+            n -= ++c;
+        }
+        return c;
+    }
 }
