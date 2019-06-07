128f7eede258aecc63dbacd236b94fe6b1d07b58
yujingwen <lection.yu@gmail.com>
2019-06-08 02:08:23 +0800

    88 非原地代码优化至1行

--- Week_01/id_3/array/88/lc_88_v2.py
+++ Week_01/id_3/array/88/lc_88_v2.py
@@ -1,7 +1,5 @@
-# 题目未要求原地排序 所以弄个简单的
+# 题目并未要求原地排序 所以弄个简单的
 
 
 def merge(nums1, m, nums2, n):
-    r = nums1[:m] + nums2[:n]
-    r.sort()
-    nums1[:m+n] = r
+    nums1[:m+n] = sorted(nums1[:m] + nums2[:n])
