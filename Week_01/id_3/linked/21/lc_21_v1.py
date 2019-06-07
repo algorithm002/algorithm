5b929a88d003f934e739b1c706bd4c04135f62bc
yujingwen <lection.yu@gmail.com>
2019-06-07 18:25:19 +0800

    21 用更python的方式简化代码

--- Week_01/id_3/linked/21/lc_21_v1.py
+++ Week_01/id_3/linked/21/lc_21_v1.py
@@ -1,23 +1,16 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 def merge(l1, l2):
-    root = None
-    tmp = None
+    cur = root = ListNode(0)
     while l1 and l2:
         if l1.val > l2.val:
-            _t = l2
-            l2 = l2.next
-        else:
-            _t = l1
-            l1 = l1.next
-
-        if not root:
-            root = _t
-        else:
-            tmp.next = _t
-        tmp = _t
-
-    if tmp:
-        tmp.next = l1 or l2
-    else:
-        root = l1 or l2
-
-    return root
+            l1, l2 = l2, l1
+        cur.next = l1
+        cur = l1
+        l1 = l1.next
+    cur.next = l1 or l2
+    return root.next
--- Week_01/id_3/linked/21/lc_21_v2.py
+++ Week_01/id_3/linked/21/lc_21_v2.py
@@ -1,12 +1,11 @@
+# 本代码按python语法进行优化 查看之前得提交可以看到更远古的写法
+
+
 def merge(l1, l2):
-    if l1 and l2:
-        if l1.val < l2.val:
-            r = l1
-            l1 = l1.next
-        else:
-            r = l2
-            l2 = l2.next
-        r.next = merge(l1, l2)
-        return r
-    else:
+    if not l1 or not l2:
         return l1 or l2
+
+    if l1.val > l2.val:
+        l1, l2 = l2, l1
+    l1.next = merge(l1.next, l2)
+    return l1
