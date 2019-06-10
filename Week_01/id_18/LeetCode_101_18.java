73759693819da444d8329d21be68776ccb768e79
ChenYue <liveforexperience@126.com>
2019-06-10 19:27:21 +0800

    新增101题1解

--- Week_01/id_18/LeetCode_101_18.java
+++ Week_01/id_18/LeetCode_101_18.java
@@ -1,5 +1,8 @@
 package Week_01.id_18;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author LiveForExperience
  * @date 2019/6/10 18:52
@@ -19,6 +22,35 @@ public class LeetCode_101_18 {
         }
 
         return (left.val == right.val) && doCheck(left.left, right.right) && doCheck(left.right, right.left);
+    }
+
+    public boolean isSymmetric1(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if (left == null && right == null) {
+                continue;
+            }
+
+            if (left == null || right == null) {
+                return false;
+            }
+
+            if (left.val != right.val) {
+                return false;
+            }
+
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
     }
 
     private class TreeNode {
