d2970667996e7664ebe66290e0e19cbf320a73a0
ChenYue <liveforexperience@126.com>
2019-06-10 20:39:44 +0800

    新增101题1解

--- Week_01/id_18/LeetCode_101_18.java
+++ Week_01/id_18/LeetCode_101_18.java
@@ -2,6 +2,7 @@ package Week_01.id_18;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * @author LiveForExperience
@@ -49,6 +50,35 @@ public class LeetCode_101_18 {
             queue.add(right.right);
             queue.add(left.right);
             queue.add(right.left);
+        }
+        return true;
+    }
+
+    public boolean isSymmetric2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode left = stack.pop();
+            TreeNode right = stack.pop();
+
+            if (left == null && right == null) {
+                continue;
+            }
+
+            if (left == null || right == null) {
+                return false;
+            }
+
+            if (left.val != right.val) {
+                return false;
+            }
+
+            stack.push(left.left);
+            stack.push(right.right);
+            stack.push(left.right);
+            stack.push(right.left);
         }
         return true;
     }
