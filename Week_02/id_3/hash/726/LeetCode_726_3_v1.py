845f675744570bedacb5b99b9aae299c29de4384
yujingwen <lection.yu@gmail.com>
2019-06-13 03:32:45 +0800

    726 加入递归版本

--- Week_02/id_3/hash/726/LeetCode_726_3_v1.py
+++ Week_02/id_3/hash/726/LeetCode_726_3_v1.py
@@ -71,8 +71,3 @@ class Solution:
         m2.clear()
         return m1
 
-
-s1 = Solution()
-print(s1.countOfAtoms('H2O') == 'H2O')
-print(s1.countOfAtoms('Mg(OH)2') == 'H2MgO2')
-print(s1.countOfAtoms('K4(ON(SO3)2)2') == 'K4N2O14S4')
