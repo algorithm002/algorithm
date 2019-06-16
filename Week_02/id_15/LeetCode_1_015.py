5df911ff56f77b599713a39d01685dcac35964f6
Bruno Yang <bruno_yang0918@163.com>
2019-06-16 20:25:02 +0800

    Create LeetCode_1_015.py

---
+++ Week_02/id_15/LeetCode_1_015.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d={}
+        for i,num in enumerate(nums):
+            if target-num in d:
+                return d[target-num], i
+            d[num]=i
