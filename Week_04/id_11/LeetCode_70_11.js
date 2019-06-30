9e666318d82081815d2a24ea1805e92bc791cfc6
jianzheng <jianzheng@paypal.com>
2019-06-30 23:10:15 +0800

    第四周作业#011

---
+++ Week_04/id_11/LeetCode_70_11.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    
+
+    let s = []
+
+    if(n == 0) return 1
+    if(n == 1) return 1
+    s[0] = 1
+    s[1] = 1
+    for(let i = 2; i<= n; i++){
+        s[i] = s[i-1] + s[i-2]
+    }
+    return s[n]
+};
+
+
