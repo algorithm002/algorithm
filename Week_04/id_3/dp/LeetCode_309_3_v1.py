3e01e9a45a773f241c17e641c8702516be29dd86
yujingwen <lection.yu@gmail.com>
2019-07-10 03:11:31 +0800

    309 完成递归+缓存

--- Week_04/id_3/dp/LeetCode_309_3_v1.py
+++ Week_04/id_3/dp/LeetCode_309_3_v1.py
@@ -14,7 +14,15 @@
 =======================
 DP还是不熟，需要可以练习，先从暴力解法开始。
 status 0不操作 1买 2卖
+在recursion三个参数 下标 上一次的状态 数据
+三种状态三种不同的下钻方式
+技巧在于买入不卖的等待状态相当于每次的累加 每一步的状态都可以计算 便于缓存
+price[5]-price[2] = (prices[3]-price[2])+(prices[4]-price[3])...
 =======================
+加入缓存 参数其实就是状态 当前下标配合当前状态构成了一个缓存
+注意 二维数组初始化不可以这么写 [[0,0,0]]*n 这样所有数组的子元素都引用的同一个二级数组 [0,0,0]
+
+完美，一条过 打败93%
 """
 
 
@@ -22,25 +30,30 @@ class Solution:
     def maxProfit(self, prices) -> int:
         if not prices:
             return 0
-        return self.recursion(0, 0, prices)
+        return self.recursion(0, 0, prices, [[None, None, None] for _ in range(len(prices))])
 
-    def recursion(self, idx, status, prices):
+    def recursion(self, idx, status, prices, cache):
         if idx >= len(prices):
             return 0
+        if cache[idx][status] is not None:
+            return cache[idx][status]
         if status == 0:
-            return max(
-                self.recursion(idx+1, 0, prices),
-                self.recursion(idx+1, 1, prices)
+            r = max(
+                self.recursion(idx+1, 0, prices, cache),
+                self.recursion(idx+1, 1, prices, cache)
             )
-        if status == 1:
-            return (prices[idx] - prices[idx-1]) + max(
-                self.recursion(idx+1, 1, prices),
-                self.recursion(idx+1, 2, prices),
+        elif status == 1:
+            r = (prices[idx] - prices[idx-1]) + max(
+                self.recursion(idx+1, 1, prices, cache),
+                self.recursion(idx+1, 2, prices, cache),
             )
-        if status == 2:
-            return self.recursion(idx+1, 0, prices)
+        elif status == 2:
+            r = self.recursion(idx+1, 0, prices, cache)
+        cache[idx][status] = r
+        return r
 
 
 s = Solution()
 print(s.maxProfit([1, 2, 3, 0, 2, 4, 5, 7, 8]) == 9)
 print(s.maxProfit([1, 2, 3, 0, 2]) == 3)
+print(s.maxProfit([1, 2, 3, -2, 5, 0, 2]) == 8)
